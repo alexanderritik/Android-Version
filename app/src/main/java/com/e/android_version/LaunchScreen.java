7df83bb26e2bc23ee3e59123eba1dfcceaef230a
Mayank Maurya <54510172+mayank-beast@users.noreply.github.com>
2020-09-03 15:55:40 +0530

    Flash Screen Animation Done!

--- app/src/main/java/com/e/android_version/LaunchScreen.java
+++ app/src/main/java/com/e/android_version/LaunchScreen.java
@@ -2,7 +2,17 @@ package com.e.android_version;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class LaunchScreen extends AppCompatActivity {
 
@@ -10,6 +20,27 @@ public class LaunchScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+
+        TextView tv=findViewById(R.id.dialog);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splashtrans);
+        tv.startAnimation(animation);
+
+        final Intent i = new Intent(this, MainActivity.class);
+
+        Thread timer=new Thread(){
+          public void run(){
+              try{
+                  sleep(3000);
+              }catch (InterruptedException e){
+                  e.printStackTrace();
+              }finally {
+                  startActivity(i);
+                  finish();
+              }
+          }
+        };
+        timer.start();
+
 
 
     }
